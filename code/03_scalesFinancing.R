099f7afce56ef42b0304f1fef372b2226f9fa241
pacheco-andrea <pacheco.gracia@gmail.com>
2024-04-04 21:08:15 +0200

    figured out treemap cols

--- code/03_scalesFinancing.R
+++ code/03_scalesFinancing.R
@@ -37,16 +37,22 @@ head(bd_fin)
 # note: actually should not say nature positive but rather financing to NbS according to 2023 SFN
 positiveData <- read.csv("data/positiveFinancialFlows_clean.csv")
 
-positiveTreemapData <- positiveData %>% filter(Source == "UNEP 2023 SFN")
+positiveTreemapData <- positiveData %>% filter(Source == "UNEP 2023 SFN") %>%
+  select(c("id", "Sector", "Categ_instrmnt", "Sector_econAct", "Value_lowerLim", "Value_upperLim", 
+           "YearData", "NormalizedValue_YY", "Source", "meanUSD_Y"))
+positiveTreemapData$Sector_econAct <- str_to_sentence(positiveTreemapData$Sector_econAct)
+positiveTreemapData$Sector_econAct <- gsub("International (odas and other financial flows)", "ODA", positiveTreemapData$Sector_econAct, fixed = TRUE)
+positiveTreemapData$Sector_econAct <- gsub("Total nbs", "Total NbS", positiveTreemapData$Sector_econAct, fixed = TRUE)
+positiveTreemapData$Categ_instrmnt <- gsub("Philanthropy/NGOs", "Philanthropy/ NGOs", positiveTreemapData$Categ_instrmnt)
+  
 positiveTreemapData$label <- NA
-
 positiveTreemapData$label[which(positiveTreemapData$Sector == "Private")] <- paste0(positiveTreemapData$Categ_instrmnt[which(positiveTreemapData$Sector == "Private")],
                                                                                     " ($", positiveTreemapData$meanUSD_Y[which(positiveTreemapData$Sector == "Private")], "B)")
 positiveTreemapData$label[which(positiveTreemapData$Sector == "Public")] <- paste0(positiveTreemapData$Sector_econAct[which(positiveTreemapData$Sector == "Public")],
                                                                                     " ($", positiveTreemapData$meanUSD_Y[which(positiveTreemapData$Sector == "Public")], "B)")
 
 setwd("G:/My Drive/Projects/IPBES-Nexus/00_analyses/finFlows_nexus/outputs/")
-png(filename = "posFlows.png", width = 30, height = 14, units = "cm", res = 300, bg = "white")
+svg(filename = "posFlows_versionTranslation.svg", width = 7, height = 3.5, bg = "white")
 
 treemap(positiveTreemapData,
         index = c("Sector", "label"),
@@ -58,17 +64,40 @@ treemap(positiveTreemapData,
           c("right", "bottom")),  
         # position.legend = "right",
         # reverse.legend = T,
-        border.col = "white",
+        border.col = "gray20",
         border.lwds = c(1,.5,.05),
-        fontsize.labels = c(20,12),
+        fontsize.labels = c(18,13),
+        fontcolor.labels ="transparent",
+        fontsize.title = 22,
+        palette = c("#C6D68A", "#D9AA80"),
+        overlap.labels=0.5,   # number between 0 and 1 that determines the tolerance of the overlap between labels. 0 means that labels of lower levels are not printed if higher level labels overlap, 1  means that labels are always printed. In-between values, for instance the default value .5, means that lower level labels are printed if other labels do not overlap with more than .5  times their area size.
+        title = "                                      ")
+dev.off()
+
+setwd("G:/My Drive/Projects/IPBES-Nexus/00_analyses/finFlows_nexus/outputs/")
+png(filename = "posFlows_versionLabels.png", width = 30, height = 18, units = "cm", res = 300, bg = "white")
+
+treemap(positiveTreemapData,
+        index = c("Sector", "label"),
+        vSize = "meanUSD_Y", 
+        type = "index",
+        bg.labels=c("transparent"),
+        align.labels=list(
+          c("left", "top"), 
+          c("right", "bottom")),  
+        # position.legend = "right",
+        # reverse.legend = T,
+        border.col = "gray20",
+        border.lwds = c(1,.5,.05),
+        fontsize.labels = c(18,12),
         fontcolor.labels ="gray20",
         fontsize.title = 22,
         palette = c("#C6D68A", "#D9AA80"),
+        force.print.labels = T, 
         overlap.labels=0.5,   # number between 0 and 1 that determines the tolerance of the overlap between labels. 0 means that labels of lower levels are not printed if higher level labels overlap, 1  means that labels are always printed. In-between values, for instance the default value .5, means that lower level labels are printed if other labels do not overlap with more than .5  times their area size.
         title = "Public and private financing towards NbS (Billions USD/year)")
 
 dev.off()
-
 # treemap of all nature-neg activities ----
 
 # after the first version where I was piecing together several data sources, 
@@ -92,120 +121,67 @@ neg_illeg <- bd_fin %>%
   filter(Categ_impact == "Negative") %>%
   select(c("id", "Sector", "Categ_instrmnt", "Sector_econAct", "Value_lowerLim", "Value_upperLim", 
            "YearData", "NormalizedValue_YY", "Source"))
-neg_illeg <- neg_illeg[grep("Illegal", neg_illeg$Categ_instrmnt),]
+# looked at this in detail, and found that the 110-281 estimate is the total value, so we should just use this
+neg_illeg <- neg_illeg[grep("wildlife trade, mining, forestry and fishing", neg_illeg$Sector_econAct),]
 
 # make table with all negative flows
-neg_flow_clean <- rbind(neg_flow, neg_illeg)
-nrow(neg_flow_clean)
-neg_flow_clean
+negativeTreemapData <- rbind(neg_flow, neg_illeg)
+nrow(negativeTreemapData)
+negativeTreemapData$Sector_econAct <- str_to_sentence(negativeTreemapData$Sector_econAct)
 
+# fill in the NAs to prevent error in the labelmaking function
+negativeTreemapData$Value_upperLim[which(is.na(negativeTreemapData$Value_upperLim))] <- negativeTreemapData$Value_lowerLim[which(is.na(negativeTreemapData$Value_upperLim))]
 # create mean value for plotting
-neg_flow_clean$mValue <- rowMeans(cbind(neg_flow_clean$Value_lowerLim, neg_flow_clean$Value_upperLim), na.rm = T)
+negativeTreemapData$meanUSD_Y <- rowMeans(cbind(negativeTreemapData$Value_lowerLim, negativeTreemapData$Value_upperLim), na.rm = T)
 
 # create a label that will allow me to paste the range in values, but specify if condition for only when there is a range
-neg_flow_clean$label <- NA
+negativeTreemapData$label <- NA
 makeLabelWithLims <- function(table, labelName){
   n <- grep(labelName, colnames(table))
   labels <- c()
   for(i in 1:nrow(table))
   {
     labels[i] <- paste0(table[i,n],
-                        if(table$Value_lowerLim[i] < table$Value_upperLim[i], 
-                           
-                           
-                           na.rm = T){
+                        if(table$Value_lowerLim[i] < table$Value_upperLim[i]){
                           paste0(" ($", table$Value_lowerLim[i], "-", table$Value_upperLim[i], " B)")
-                        }else{paste0(" ($", table$mValue[i], " B)")},
+                        }else{paste0(" ($", table$meanUSD_Y[i], " B)")},
                         sep = "\n")
   }
   return(labels)
 }
-neg_flow_clean$label <- makeLabelWithLims(neg_flow_clean, "Sector_econAct")
+negativeTreemapData$label <- makeLabelWithLims(negativeTreemapData, "Sector_econAct")
 
-
-# need 5 colors
-
-# write out treemap
-# setwd("G:/My Drive/Projects/IPBES-Nexus/analyses/fin_flows/outputs/")
-# png(filename = "negFlows.png", width = 1000, height = 600, units = "px", bg = "white")
-treemap(neg_flow_clean, 
-        index = c("Categ_instrmnt", "label"), # the order determines groups and subgroups
-        vSize = "mValue",
+# don't really use the negative flows in the chapter (separately)
+treemap(negativeTreemapData, 
+        index = c("Categ_instrmnt", "label"), 
+        vSize = "meanUSD_Y",
         type = "index",
         bg.labels=c("transparent"),
         align.labels=list(
           c("left", "top"), 
           c("center", "center")
         ),  
-        # position.legend = "right",
-        # reverse.legend = T,
         fontsize.labels = c(14,12),
         fontsize.title = 16,
         palette = "YlOrRd",
-        overlap.labels=0.5,   # number between 0 and 1 that determines the tolerance of the overlap between labels. 0 means that labels of lower levels are not printed if higher level labels overlap, 1  means that labels are always printed. In-between values, for instance the default value .5, means that lower level labels are printed if other labels do not overlap with more than .5  times their area size.
+        overlap.labels=0.5,   
         title = "Annual public and private financial support for nature-negative activities (Billions USD)")
-# dev.off()
 
 
-# treemap scaled up for all financial flows ----
+# treemap positive and negative financial flows ----
 
 # to join positive and negative flows:
+# need to add back in these directions of impact
+positiveTreemapData$Categ_impact <- "Positive"
+negativeTreemapData$Categ_impact <- "Negative"
+colnames(positiveTreemapData) == colnames(negativeTreemapData)
+allFlows <- rbind(positiveTreemapData, negativeTreemapData)
 
-# 1. clean up positive flows 
-pos_flow2 <- pos_flow[which(pos_flow$Source == "UNEP 2022 (State of Finance)"),]
-pos_flow2 <- pos_flow2[which(pos_flow2$Sector_econAct != "total"),]
-pos_flow2 <- pos_flow2[which(pos_flow2$Sector_econAct != "marine"),] # need to exclude marine because its double counting, but it should be mentioned in the text
-# create mean value for plotting 
-pos_flow2$mValue <-  rowMeans(cbind(pos_flow2$Value_lowerLim, pos_flow2$Value_upperLim))
-# create column for labeling
-# in this case, i want a composite label of the category of instrument and of the sector of econ activity
-pos_flow2$Categ_instrmnt <- gsub("Domestic budgets", "Domestic budgets:", pos_flow2$Categ_instrmnt)
-pos_flow2$categ3 <- paste(pos_flow2$Categ_instrmnt, pos_flow2$Sector_econAct)
-pos_flow2$label <- makeLabelWithLims(pos_flow2, "categ3")
-# visualize
-treemap(pos_flow2, 
-        index = c("Sector", "label"), # the order determines groups and subgroups
-        vSize = "mValue",
-        type = "index",
-        bg.labels=c("transparent"),
-        align.labels=list(
-          c("left", "top"), 
-          c("center", "center")
-        ),  
-        fontsize.labels = c(14,12),
-        fontsize.title = 16,
-        palette = "Greens",
-        overlap.labels=0.5,   # number between 0 and 1 that determines the tolerance of the overlap between labels. 0 means that labels of lower levels are not printed if higher level labels overlap, 1  means that labels are always printed. In-between values, for instance the default value .5, means that lower level labels are printed if other labels do not overlap with more than .5  times their area size.
-        title = "Annual public and private financial support for nature-positive activities (Billions USD)")
-
-# because positive flows are so much smaller than negative flows, 
-# plotting the small details of the different instruments via which private flows are dispersed doesn't make sense 
-# these details will be unreadable in the treemap
-# instead, i chose to create a simplified row/observation for positive flows to add to the negative flow table
-# this should illustrate the scale of financing of positive compared to negative
-
-# join positive and negative flows:
-neg_flow_clean
-colnames(neg_flow_clean) == colnames(pos_flow2[,-c(17)])
-pos_flow3 <- pos_flow2 %>%
-  group_by(Category, Categ_impact, Sector, Unit..USD.YY., NormalizedValue_YY, Source) %>%
-  summarize(Value_lowerLim = sum(Value_lowerLim),
-            Value_upperLim = sum(Value_upperLim)) 
-# need to add label column at this point
-pos_flow3$label <- makeLabelWithLims(pos_flow3, "Sector")
-
-# join into table of all flows
-posWithinNegFlows <- full_join(neg_flow_clean, pos_flow3)
-# recalculate mean values to include 2 new ones
-posWithinNegFlows$mValue <- rowMeans(cbind(posWithinNegFlows$Value_lowerLim, posWithinNegFlows$Value_upperLim))
-
-
-setwd("G:/My Drive/Projects/IPBES-Nexus/00_analyses/finFlows_nexus/outputs/")
-png(filename = "summaryAllFlows.png", width = 1100, height = 650, units = "px", bg = "white")
-
-treemap(posWithinNegFlows, 
-        index = c( "Sector", "Categ_impact", "Categ_instrmnt", "label"), # the order determines groups and subgroups
-        vSize = "mValue",
+# try the treemap with all flows
+treemap(allFlows, 
+        index = c("Categ_impact", "Sector", "label"), 
+        vSize = "meanUSD_Y",
+        vColor = "manual",
         type = "index",
         bg.labels=c("transparent"),
         align.labels=list(
@@ -214,50 +190,78 @@ treemap(posWithinNegFlows,
           c("right", "bottom")),
         ymod.labels = c(5,0,0),
         position.legend = "bottom",
-        border.col = "white",
+        border.col = "gray20",
         border.lwds = c(1,.5,.05),
         fontsize.labels = c(28,22,16),
-        fontcolor.labels ="white",
+        fontcolor.labels ="gray20",
         fontsize.title = 33,
         fontsize.legend = 22,
+        palette = list("Categ_impact" = c("Negative"= "#D9AA80", "Positive"= "#4A928F"),
+                       "Sector" = c("Public"= "#BAB0C9", "Private"= "#C6D68A", "Other" = "yellow")),
+        overlap.labels=0.5,   
+        title = "Scale of annual nature-negative and nature-positive financing (Billions USD)")
+# because positive flows are so much smaller than negative flows, 
+# plotting the small details of the different instruments via which private flows are dispersed doesn't make sense 
+# these details will be unreadable in the treemap
+# instead, it's best to create a simplified row/observation for positive flows to add to the negative flow table
+# this should illustrate the scale of financing of positive compared to negative
+allFlows
+nx_col <- c("#799336","#A0AF67","#C6D68A",     
+            "#196C71","#4A928F","#A7C6C5",     
+            "#4D2D71","#8B78A1","#BAB0C9",    
+            "#791E32",
+            "#B65719","#C3773E",
+            "#D9AA80","#696B5F","#ACABA4","#FFFFFF") 
+
+
+# edit the positive data accordingly
+positiveTreemapData %>%
+  group_by(Sector) %>%
+  summarize(meanUSD_Y = sum(meanUSD_Y)) 
+positiveTreemapData2 <- data.frame(matrix(ncol = ncol(positiveTreemapData), nrow = 2))
+names(positiveTreemapData2) <- names(positiveTreemapData)
+positiveTreemapData2 <- right_join(positiveTreemapData2, as.data.frame(positiveTreemapData %>%  group_by(Sector) %>%  summarize(meanUSD_Y = sum(meanUSD_Y))))
+# add the label once again
+positiveTreemapData2$label <- paste0(" ($", positiveTreemapData2$meanUSD_Y, "B)") # dont need to add the category
+positiveTreemapData2$Categ_impact <- "Positive"
+colnames(positiveTreemapData) == colnames(negativeTreemapData)
+allFlows2 <- rbind(positiveTreemapData2, negativeTreemapData)
+
+# i need to make a factor column for the colors
+allFlows2$color <- NA
+allFlows2$color[which(allFlows2$Categ_impact == "Positive")] <- "pos"
+allFlows2$color[which(allFlows2$Categ_impact == "Negative" & allFlows2$Sector == "Private")] <- "neg-pri"
+allFlows2$color[which(allFlows2$Categ_impact == "Negative" & allFlows2$Sector == "Public")] <- "neg-pub"
+allFlows2$color[which(allFlows2$Categ_impact == "Negative" & allFlows2$Sector == "Other")] <- "neg-oth"
+allFlows2$color <- as.factor(allFlows2$color)
+
+setwd("G:/My Drive/Projects/IPBES-Nexus/00_analyses/finFlows_nexus/outputs/")
+png(filename = "summaryAllFlows.png",  width = 30, height = 18, units = "cm", res = 300, bg = "white")
+
+treemap(allFlows2, 
+        index = c("Categ_impact", "Sector", "label"), # the order determines groups and subgroups
+        vSize = "meanUSD_Y",
+        vColor = "color",
+        type = "categorical",
+        bg.labels=c("transparent"),
+        align.labels=list(
+          c("left", "top"),
+          c("left", "top"), 
+          c("right", "bottom")),
+        ymod.labels = c(5,0,0),
+        position.legend = "none",
+        border.col = "gray20",
+        border.lwds = c(1,.5,.05),
+        fontsize.labels = c(18,15,12),
+        fontcolor.labels ="gray20",
+        fontsize.title = 22,
+        force.print.labels = T,
+        # fontsize.legend = 22,
         # palette = c("#D9AA80", "#196C71"),
-        palette = c("#B65719", "#4A928F", "#C6D68A", "#4D2D71", "#696B5F", "#FFFFFF"),
+        palette = c("#EDD018","#C3773E", "#D9AA80", "#4A928F"), #, "#4D2D71", "#696B5F", "#FFFFFF"),
         overlap.labels=0.5,   
         title = "Scale of annual nature-negative and nature-positive financing (Billions USD)")
 
 dev.off()
 
-# positive flows ----
-pos_flow2
-
-# potential treemap
-setwd("G:/My Drive/Projects/IPBES-Nexus/00_analyses/finFlows_nexus/outputs/")
-png(filename = "posFlows.png", width = 1100, height = 650, units = "px", bg = "white")
-
-treemap(pos_flow2,
-        index = c("Sector", "label"),
-        vSize = "mValue", 
-        type = "index",
-        bg.labels=c("transparent"),
-        align.labels=list(
-          c("left", "top"), 
-          c("right", "bottom")
-        ),  
-        # position.legend = "right",
-        # reverse.legend = T,
-        border.col = "white",
-        border.lwds = c(1,.5,.05),
-        fontsize.labels = c(28,20,18),
-        fontcolor.labels ="white",
-        fontsize.title = 28,
-        palette = c("#C6D68A", "#4A928F"),
-        overlap.labels=0.5,   # number between 0 and 1 that determines the tolerance of the overlap between labels. 0 means that labels of lower levels are not printed if higher level labels overlap, 1  means that labels are always printed. In-between values, for instance the default value .5, means that lower level labels are printed if other labels do not overlap with more than .5  times their area size.
-        title = "Annual public and private financial support for nature-positive activities (Billions USD)")
-
-dev.off()
-
-# quick calculation of fig
-pos_flow2 %>%
-  group_by(Sector) %>%
-  summarize(sumsLower = sum(Value_lowerLim), sumsUpper = sum(Value_upperLim))
 
